0308e97b564b1760187f221a43f2f1ce1b544728
Bhanu Singh <122738012+bhanusingh008@users.noreply.github.com>
2023-05-04 03:58:10 +0530

    Update Ecommerce.java

--- Ecommerce.java
+++ Ecommerce.java
@@ -3,6 +3,7 @@ package com.e_commerce.ecommerce;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
